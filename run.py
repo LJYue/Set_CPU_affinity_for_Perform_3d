a3b91ec477ab2f08b92ac9d16226cacb3ab52ef0
LJYue <linjinyue020@gmail.com>
2017-02-21 18:08:20 +0800

    Prints time in local timezone. Fixed the possible bug of threads-depeendent issue Requires psutill 5.1+

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import time
 import psutil
 
 s = sched.scheduler(time.time, time.sleep)
-def change_affinity(sc): 
+def set_affinity(sc): 
     all_id = psutil.pids()
     p3d_id = []
     for i in all_id:
@@ -11,11 +11,11 @@ def change_affinity(sc):
         if "Pf3dEngine" in p.name():
     #       print p.pid
     #       break
-            p.cpu_affinity([0,1,2,3,4,5,6,7,8,9,10,11])
+            p.cpu_affinity([])
             p.nice(psutil.HIGH_PRIORITY_CLASS)
             print str(p.pid)+" set"
-    print time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-    s.enter(600, 1, change_affinity, (s,))
+    print time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    s.enter(600, 1, set_affinity, (s,))
 
-s.enter(1, 1, change_affinity, (s,))
+s.enter(1, 1, set_affinity, (s,))
 s.run()
