88e6c52727043f6d9c307a4ab8f855ad14741084
LJYue <linjinyue020@gmail.com>
2017-02-22 13:59:15 +0800

    now sets priority for Perform-3d main process as well

--- run.py
+++ run.py
@@ -2,20 +2,23 @@ import sched
 import time
 import psutil
 
-s = sched.scheduler(time.time, time.sleep)
-def set_affinity(sc): 
-    all_id = psutil.pids()
-    p3d_id = []
-    for i in all_id:
-        p = psutil.Process(i)
-        if "Pf3dEngine" in p.name():
-    #       print p.pid
-    #       break
-            p.cpu_affinity([])
-            p.nice(psutil.HIGH_PRIORITY_CLASS)
-            print str(p.pid)+" set"
-    print time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    s.enter(600, 1, set_affinity, (s,))
+def main():
+    s = sched.scheduler(time.time, time.sleep)
+    def set_affinity(sc): 
+        all_id = psutil.pids()
+        for i in all_id:
+            p = psutil.Process(i)
+            if "Pf3dEngine" in p.name()or "Perform-3D" in p.name():
+        #       print p.pid
+        #       break
+                p.cpu_affinity([])
+                p.nice(psutil.HIGH_PRIORITY_CLASS)
+                print str(p.pid)+" set"
+        print time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        s.enter(600, 1, set_affinity, (s,))
 
-s.enter(1, 1, set_affinity, (s,))
-s.run()
+    s.enter(1, 1, set_affinity, (s,))
+    s.run()
+
+if __name__ == "__main__":
+    main()
